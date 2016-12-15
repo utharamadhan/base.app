030e33820f9f4f63e361f1a78a802e607585c0de
utha <rizki_utha@infoflow.co.id>
2016-12-15 21:32:30 +0700

    push base app read

--- Web/WebContent/menuPage.jsp
+++ Web/WebContent/menuPage.jsp
@@ -17,56 +17,19 @@
 			                        	<i class="fa fa-caret-right"></i> System Parameter
 			                        </a>
 			                  	</li>
-			                  	<li id="reportstok">
+			                  	<li id="lookup">
 									<a href="/Web/do/lookupGroup/showList">
 			                        	<i class="fa fa-caret-right"></i> Lookup
 			                        </a>
 			                  	</li>
-							</ul>
-	                  	</li>
-	                  	<li id="stock">
-	                    	<a href="/Web/do/stock/showList">
-	                      		<i class="fa fa-cubes"></i> Stok
-	                    	</a>
-	                  	</li>
-	                  	<li id="pengolahan">
-	                  		<a href="/Web/do/pengolahan/showCurrentMonth">
-	                      		<i class="fa fa fa-hourglass"></i> Pengolahan
-	                    	</a>
-	                  	</li>
-	                  	<li id="penjualan">
-	                    	<a href="/Web/do/penjualan/showList">
-	                      		<i class="fa fa-tags"></i> Penjualan
-	                    	</a>
-	                  	</li>
-	                  	<li id="reports" class="dropdown">
-	                  		<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                     			<i class="fa fa-bar-chart"></i> Laporan <b class="fa fa-plus dropdown-plus"></b>
-                   			</a>
-							<ul class="dropdown-menu">
-								<li id="reportpenerimaan">
-									<a href="/Web/do/reports/penerimaan/showList">
-			                        	<i class="fa fa-caret-right"></i> Penerimaan
+			                  	<li id="userMaintenance">
+									<a href="/Web/do/userMaintenance/showList">
+			                        	<i class="fa fa-caret-right"></i> User
 			                        </a>
 			                  	</li>
-			                  	<li id="reportstok">
-									<a href="/Web/do/reports/stock/showList">
-			                        	<i class="fa fa-caret-right"></i> Inventori Stok
-			                        </a>
-			                  	</li>
-			                  	<li id="reportjualbeli">
-									<a href="/Web/do/reports/analisajualbeli/showList">
-			                        	<i class="fa fa-caret-right"></i> Biaya - Biaya
-			                        </a>
-			                  	</li>
-			                  	<li id="reportcashflow">
-									<a href="/Web/do/reports/cashflow/showList">
-			                        	<i class="fa fa-caret-right"></i> Cash Flow
-			                        </a>
-			                  	</li>
-			                  	<li id="reportpenjualan">
-									<a href="/Web/do/reports/penjualan/showList">
-			                        	<i class="fa fa-caret-right"></i> Cash Flow
+			                  	<li id="appRoleMaintenance">
+									<a href="/Web/do/appRoleMaintenance/showList">
+			                        	<i class="fa fa-caret-right"></i> Role
 			                        </a>
 			                  	</li>
 							</ul>
--- Web/src/id/base/app/webMember/administrator/UserMaintenanceWebController.java
+++ Web/src/id/base/app/webMember/administrator/UserMaintenanceWebController.java
@@ -12,6 +12,7 @@ import id.base.app.util.dao.Operator;
 import id.base.app.util.dao.SearchFilter;
 import id.base.app.util.dao.SearchOrder;
 import id.base.app.valueobject.AppParameter;
+import id.base.app.valueobject.AppUser;
 import id.base.app.webMember.DataTableCriterias;
 import id.base.app.webMember.controller.BaseController;
 
@@ -32,15 +33,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Scope(value="request")
 @Controller
-@RequestMapping("/systemParameter")
-public class SystemParameterWebController extends BaseController<AppParameter> {
+@RequestMapping("/userMaintenance")
+public class UserMaintenanceWebController extends BaseController<AppUser> {
 
-	private final String PATH_LIST = "/administration/systemParameterList";
-	private final String PATH_DETAIL = "/administration/systemParameterDetail";
+	private final String PATH_LIST = "/administration/userMaintenanceList";
+	private final String PATH_DETAIL = "/administration/userMaintenanceDetail";
 	
 	@Override
-	protected RestCaller<AppParameter> getRestCaller() {
-		return new RestCaller<AppParameter>(RestConstant.REST_SERVICE, RestServiceConstant.SYSTEM_PARAMETER_SERVICE);
+	protected RestCaller<AppUser> getRestCaller() {
+		return new RestCaller<AppUser>(RestConstant.REST_SERVICE, RestServiceConstant.USER_SERVICE);
 	}
 
 	@Override
@@ -49,7 +50,7 @@ public class SystemParameterWebController extends BaseController<AppParameter> {
 	}
 	
 	private void setDefaultFilter(HttpServletRequest request, List<SearchFilter> filters) {
-		filters.add(new SearchFilter(AppParameter.IS_VIEWABLE, Operator.EQUALS, Boolean.TRUE));
+		filters.add(new SearchFilter(AppUser.STATUS, Operator.EQUALS, SystemConstant.ValidFlag.VALID));
 	}
 
 	@Override
@@ -57,7 +58,7 @@ public class SystemParameterWebController extends BaseController<AppParameter> {
 		List<SearchFilter> filters = new ArrayList<>();
 		setDefaultFilter(request, filters);
 		if(StringFunction.isNotEmpty(columns.getSearch().get(DataTableCriterias.SearchCriterias.value))){
-			filters.add(new SearchFilter(AppParameter.NAME, Operator.LIKE, columns.getSearch().get(DataTableCriterias.SearchCriterias.value)));
+			filters.add(new SearchFilter(AppUser.USER_NAME, Operator.LIKE, columns.getSearch().get(DataTableCriterias.SearchCriterias.value)));
 		}
 		return filters;
 	}
@@ -67,19 +68,17 @@ public class SystemParameterWebController extends BaseController<AppParameter> {
 		if(orders != null) {
 			orders.clear();
 		}
-		orders.add(new SearchOrder(AppParameter.NAME, SearchOrder.Sort.DESC));
+		orders.add(new SearchOrder(AppUser.PK_APP_USER, SearchOrder.Sort.DESC));
 		return orders;
 	}
 	
 	@RequestMapping(method=RequestMethod.GET, value="showList")
 	public String showList(ModelMap model, HttpServletRequest request){
-		model.addAttribute("pagingWrapper", new PagingWrapper<AppParameter>());
+		model.addAttribute("pagingWrapper", new PagingWrapper<AppUser>());
 		return getListPath();
 	}
 	
-	public void setDefaultData(ModelMap model) {
-		model.addAttribute("dataTypeOptions", AppParameter.DATA_TYPE_LOOKUP);
-	}
+	public void setDefaultData(ModelMap model) {}
 	
 	@RequestMapping(method=RequestMethod.GET, value="showAdd")
 	public String showAdd(ModelMap model, HttpServletRequest request){
@@ -89,18 +88,18 @@ public class SystemParameterWebController extends BaseController<AppParameter> {
 	@RequestMapping(method=RequestMethod.GET, value="showEdit")
 	public String showEdit(@RequestParam(value="maintenancePK") final Long maintenancePK, @RequestParam Map<String, String> paramWrapper, ModelMap model, HttpServletRequest request){
 		setDefaultData(model);
-		AppParameter detail = getRestCaller().findById(maintenancePK);
+		AppUser detail = getRestCaller().findById(maintenancePK);
 		model.addAttribute("detail", detail);
 		return PATH_DETAIL;
 	}
 	
-	@RequestMapping(method=RequestMethod.POST, value="saveSystemParameter")
+	@RequestMapping(method=RequestMethod.POST, value="saveUser")
 	@ResponseBody
-	public Map<String, Object> saveSystemParameter(final AppParameter anObject, HttpServletRequest request) {
+	public Map<String, Object> saveUser(final AppParameter anObject, HttpServletRequest request) {
 		Map<String, Object> resultMap = new HashMap<>();
 		List<ErrorHolder> errors = new ArrayList<>();
 		try{
-			errors = new SpecificRestCaller<AppParameter>(RestConstant.REST_SERVICE, RestServiceConstant.SYSTEM_PARAMETER_SERVICE).performPut("/update", anObject);
+			errors = new SpecificRestCaller<AppParameter>(RestConstant.REST_SERVICE, RestServiceConstant.USER_SERVICE).performPut("/update", anObject);
 			if(errors != null && errors.size() > 0){
 				resultMap.put(SystemConstant.ERROR_LIST, errors);
 			}
