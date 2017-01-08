270b9e8cad8b4ee8d42f2a7897749587d0b86512
mardy jonathan <mardy@infoflow.co.id>
2017-01-09 02:51:32 +0700

    finish create news with searching and paging

--- Site/src/id/base/app/webMember/controller/site/NewsWebController.java
+++ Site/src/id/base/app/webMember/controller/site/NewsWebController.java
@@ -1,11 +1,14 @@
 package id.base.app.webMember.controller.site;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
@@ -13,10 +16,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import id.base.app.paging.PagingWrapper;
 import id.base.app.rest.RestCaller;
 import id.base.app.rest.RestConstant;
 import id.base.app.rest.RestServiceConstant;
+import id.base.app.util.dao.Operator;
 import id.base.app.util.dao.SearchFilter;
 import id.base.app.util.dao.SearchOrder;
 import id.base.app.valueobject.news.News;
@@ -33,12 +40,37 @@ public class NewsWebController {
 	}
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String view(ModelMap model, HttpServletRequest request, HttpServletResponse response){
+	public String view(ModelMap model, HttpServletRequest request, HttpServletResponse response,
+			@RequestParam(value="startNo",defaultValue="1") int startNo, 
+			@RequestParam(value="offset",defaultValue="6") int offset,
+			@RequestParam(value="filter", defaultValue="", required=false) String filterJson
+		){
 		List<SearchFilter> filter = new ArrayList<SearchFilter>();
+		if(StringUtils.isNotEmpty(filterJson)){
+			filter.add(new SearchFilter(News.TITLE, Operator.LIKE, filterJson));
+		}
 		List<SearchOrder> order = new ArrayList<SearchOrder>();
-		List<News> news = getRestCaller().findAll(filter, order);
+		PagingWrapper<News> news = getRestCaller().findAllByFilter(startNo, offset, filter, order);
 		model.addAttribute("news", news);
 		return "/news/main";
+	}
+	
+	@RequestMapping(method=RequestMethod.GET, value="/load")
+	@ResponseBody
+	public Map<String, Object> load(ModelMap model, HttpServletRequest request, HttpServletResponse response,
+			@RequestParam(value="startNo",defaultValue="1") int startNo, 
+			@RequestParam(value="offset",defaultValue="6") int offset,
+			@RequestParam(value="filter", defaultValue="", required=false) String filterJson
+		){
+		Map<String, Object> resultMap = new HashMap<>();
+		List<SearchFilter> filter = new ArrayList<SearchFilter>();
+		if(StringUtils.isNotEmpty(filterJson)){
+			filter.add(new SearchFilter(News.TITLE, Operator.LIKE, filterJson));
+		}
+		List<SearchOrder> order = new ArrayList<SearchOrder>();
+		PagingWrapper<News> news = getRestCaller().findAllByFilter(startNo, offset, filter, order);
+		resultMap.put("news", news);
+		return resultMap;
 	}
 	
 }
