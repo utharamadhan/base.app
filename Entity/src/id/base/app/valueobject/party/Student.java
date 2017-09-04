65a923ec7cf4dc52eb95c9f3686857478c1d1b00
dito <bona.dvent@gmail.com>
2017-09-04 07:06:27 +0700

    fix bug

--- Entity/src/id/base/app/valueobject/party/Student.java
+++ Entity/src/id/base/app/valueobject/party/Student.java
@@ -55,7 +55,7 @@ public class Student extends BaseEntity implements Serializable {
 	public static final String SCHOOL_ACTIVITIES_SOCIETIES = "schoolActivitiesSocieties";
 	public static final String SCHOOL_DESCRIPTION		= "schoolDescription";
 	public static final String STATUS					= "status";
-	public static final String PK_COURSE				= "studentCourses.course.pkCourse";
+	public static final String PK_COURSE				= "studentCourses.course.pkProgramItem";
 	public static final String PK_STUDENT_COURSE		= "studentCourses.pkStudentCourse";
 	
 	public static Student getInstance() {
